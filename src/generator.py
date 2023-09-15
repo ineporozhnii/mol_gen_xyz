335fadd401ac443c7cf2cddc77bdc9391742f1e9
Ihor Neporozhnii <71763551+ineporozhnii@users.noreply.github.com>
2023-09-15 19:31:48 -0400

    remove get_xyz_block from Molecule class

--- src/generator.py
+++ src/generator.py
@@ -3,7 +3,7 @@ import numpy as np
 import networkx as nx
 from typing import Union
 from sklearn.neighbors import KDTree
-from src.utils import atomic_numbers_to_symbols
+from src.utils import atomic_numbers_to_symbols, get_xyz_block
 
 class Molecule:
     """
@@ -17,16 +17,7 @@ class Molecule:
         self.atomic_numbers = atomic_numbers
         self.positions = positions
         self.atomic_symbols = [atomic_numbers_to_symbols[number] for number in atomic_numbers]
-        self.xyz_block = self._get_xyz_block()
-
-    def _get_xyz_block(self):
-        xyz_block = f"{len(self.atomic_numbers)}\n \n"
-        for i, (symbol, position) in enumerate(zip(self.atomic_symbols, self.positions)):
-            if i == len(self.atomic_numbers)-1:
-                xyz_block += f"{symbol}    {position[0]:.5f}     {position[1]:.5f}     {position[2]:.5f}"
-            else:
-                xyz_block += f"{symbol}    {position[0]:.5f}     {position[1]:.5f}     {position[2]:.5f}\n"
-        return xyz_block
+        self.xyz_block = get_xyz_block(self.atomic_symbols, self.positions)
     
 
 
